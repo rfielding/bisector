8715f2dd594d52b49295d53033d383949f7c7897
Rob Fielding <rob.fielding@gmail.com>
2015-01-28 00:08:03 -0500

    prepare to propagate restrictions

--- algebra/main.go
+++ algebra/main.go
@@ -6,6 +6,11 @@ type Expr interface {
 	isExpr()
 }
 
+type RExpr struct {
+	expr       Expr
+	constraint Expr
+}
+
 type O string
 
 type V string
@@ -28,33 +33,37 @@ type Binop struct {
 
 func (b Binop) isExpr() {}
 
-func (b Binop) Commute() (r Expr) {
-	return Binop{b.right, b.op, b.left}
+func Bop(a Expr, op O, b Expr) (r Binop) {
+	return Binop{a, op, b}
 }
 
-func (b Binop) LDistribute() (r Expr) {
+func Commute(b Binop) (r Binop) {
+	return Bop(b.right, b.op, b.left)
+}
+
+func LDistribute(b Binop) (r Binop) {
 	r = b
 	switch bi := b.right.(type) {
 	case Binop:
-		x := Binop{b.left, b.op, bi.left}
-		y := Binop{b.left, b.op, bi.right}
-		r = Binop{x, bi.op, y}
+		x := Bop(b.left, b.op, bi.left)
+		y := Bop(b.left, b.op, bi.right)
+		r = Bop(x, bi.op, y)
 	}
 	return
 }
 
-func (b Binop) RDistribute() (r Expr) {
+func RDistribute(b Binop) (r Binop) {
 	r = b
 	switch bi := b.left.(type) {
 	case Binop:
-		x := Binop{bi.left, b.op, b.right}
-		y := Binop{bi.right, b.op, b.right}
-		r = Binop{x, bi.op, y}
+		x := Bop(bi.left, b.op, b.right)
+		y := Bop(bi.right, b.op, b.right)
+		r = Bop(x, bi.op, y)
 	}
 	return
 }
 
-func (b Binop) LUnDistribute() (r Expr) {
+func LUnDistribute(b Binop) (r Binop) {
 	r = b
 	switch bl := b.left.(type) {
 	case Binop:
@@ -62,8 +71,8 @@ func (b Binop) LUnDistribute() (r Expr) {
 		case Binop:
 			if bl.left == br.left {
 				if bl.op == br.op {
-					z := Binop{bl.right, b.op, br.right}
-					r = Binop{bl.left, bl.op, z}
+					z := Bop(bl.right, b.op, br.right)
+					r = Bop(bl.left, bl.op, z)
 				}
 			}
 		}
@@ -71,7 +80,7 @@ func (b Binop) LUnDistribute() (r Expr) {
 	return
 }
 
-func (b Binop) RUnDistribute() (r Expr) {
+func RUnDistribute(b Binop) (r Binop) {
 	r = b
 	switch br := b.right.(type) {
 	case Binop:
@@ -79,8 +88,8 @@ func (b Binop) RUnDistribute() (r Expr) {
 		case Binop:
 			if bl.right == br.right {
 				if br.op == bl.op {
-					z := Binop{bl.left, b.op, br.left}
-					r = Binop{z, br.op, br.right}
+					z := Bop(bl.left, b.op, br.left)
+					r = Bop(z, br.op, br.right)
 				}
 			}
 		}
@@ -88,12 +97,37 @@ func (b Binop) RUnDistribute() (r Expr) {
 	return
 }
 
+func And(a Expr, b Expr) (r Binop) {
+	return Bop(a, "and", b)
+}
+
+func Or(a Expr, b Expr) (r Binop) {
+	return Bop(a, "or", b)
+}
+
+func Div(a Expr, b Expr) (r Binop) {
+	//constraintNew := Binop{b, "!=", I(0)}
+	return Bop(a, "/", b)
+}
+
+func Add(a Expr, b Expr) (r Binop) {
+	return Bop(a, "+", b)
+}
+
+func Mul(a Expr, b Expr) (r Binop) {
+	return Bop(a, "*", b)
+}
+
+func Eq(a Expr, b Expr) (r Binop) {
+	return Bop(a, "=", b)
+}
+
 func main() {
-	r := Binop{I(5), "/", I(4)}
-	e := Binop{V("z"), "+", r}
-	e2 := e.Commute()
-	e3 := Binop{e2, "*", I(6)}
-	e4 := e3.RDistribute()
-	e5 := Binop{V("x"), "=", e4}
+	r := Div(I(5), I(4))
+	e := Add(V("z"), r)
+	e2 := Commute(e)
+	e3 := Mul(e2, I(6))
+	e4 := RDistribute(e3)
+	e5 := Eq(V("x"), e4)
 	fmt.Println(e5)
 }
